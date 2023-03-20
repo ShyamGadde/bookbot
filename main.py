3143e2663f4ecc9bdcac461bceda8c152817b0ee
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-03-20 13:56:09 +0530

    Format with black

--- main.py
+++ main.py
@@ -4,18 +4,21 @@ def count_words_in(string):
 
 def generate_histogram_from(string):
     from collections import Counter
+
     return Counter(string.lower())
 
 
 def generate_report_from(file_contents):
-    print('--- Begin report of books/frankenstein.txt ---')
-    print(f'{count_words_in(file_contents)} words found in the document\n')
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words_in(file_contents)} words found in the document\n")
 
-    for char, count in sorted(generate_histogram_from(file_contents).items(), key=lambda x: x[1], reverse=True):
+    for char, count in sorted(
+        generate_histogram_from(file_contents).items(), key=lambda x: x[1], reverse=True
+    ):
         if char.isalpha():
             print(f"The '{char}' character was found {count} times")
 
 
-with open('books/frankenstein.txt') as f:
+with open("books/frankenstein.txt") as f:
     file_contents = f.read()
     generate_report_from(file_contents)
